b71ab48b628e6135e7f367e78e19055d9c04bc46
Andre <andre.lls@hotmail.com>
2020-10-05 10:11:00 -0400

    Add keys to gitignore

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const mongoose = require('mongoose');
 
 const app = express();
-const db = require('./config/keys').mongoURI;
+const db = require('./config/my_keys').mongoURI;
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-  
+
 app.get("/", (req, res) => res.send("Hello World"));
 
 const port = process.env.PORT || 5000;
--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://dev:gAy63ZETWxJ43uIN@cluster0.3nof1.mongodb.net/test?retryWrites=true&w=majority'
-  }
