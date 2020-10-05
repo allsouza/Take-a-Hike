9c308a3d5513e167d8444ed25cac3287dd70dceb
johnhcody <john.haner.cody@gmail.com>
2020-10-05 22:26:18 +0700

    session reducer

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -1,8 +1,6 @@
 import * as APIUtil from '../util/session_api_util';
 import jwt_decode from 'jwt-decode';
 
-// This pattern should be familiar to you from the full stack project
-
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
 
 export const logoutUser = () => ({
--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -1,16 +1,18 @@
-import * as APIUtil from '../util/session_api_util';
-import jwt_decode from 'jwt-decode';
+import { RECEIVE_USER_LOGOUT } from '../actions/session_actions';
 
-// This pattern should be familiar to you from the full stack project
-
-export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
-
-export const logoutUser = () => ({
-    type: RECEIVE_USER_LOGOUT
-});
-
-export const logout = () => dispatch => {
-    localStorage.removeItem('jwtToken')
-    APIUtil.setAuthToken(false)
-    dispatch(logoutUser())
+const initialState = {
+    isAuthenticated: false,
+    user: {}
 };
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case RECEIVE_USER_LOGOUT:
+            return {
+                isAuthenticated: false,
+                user: undefined
+            };
+        default:
+            return state;
+    }
+}
