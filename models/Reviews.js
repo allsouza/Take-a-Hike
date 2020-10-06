b835ada48c699b0c853af99cdc2b69762a067935
AAfghahi <arash.afghahi@gmail.com>
2020-10-06 14:25:37 -0400

    reviews started

---
+++ models/Reviews.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+    author:{
+        type:Schema.Types.ObjectId, 
+        ref: 'users'
+    },
+    trail:{
+        type: Schema.Types.ObjectId, 
+        ref:'trails'
+    },
+    title:{
+        type:String
+    },
+    body:{
+        type:String, 
+        text:true
+    },
+
+})
