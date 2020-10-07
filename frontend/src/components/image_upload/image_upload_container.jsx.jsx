d8e8b02747d148b7a17e0e2b31c5098242f88624
johnhcody <john.haner.cody@gmail.com>
2020-10-07 20:20:28 +0700

    adding reducers

--- frontend/src/components/image_upload/image_upload_container.jsx.jsx
+++ frontend/src/components/image_upload/image_upload_container.jsx.jsx
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux';
 import ImageUpload from './image_upload';
+import { uploadImage } from '../../actions/image_actions';
 
 const mSTP = state => ({
     
 })
 
 const mDTP = dispatch => ({
-
+    uploadImage: () => dispatch(uploadImage())
 })
 
-export default connect(null, null)(ImageUpload);
+export default connect(null, mDTP)(ImageUpload);
