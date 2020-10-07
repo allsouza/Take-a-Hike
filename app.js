7ebd84d04b239fe51c6142300b4e2676168b5997
johnhcody <john.haner.cody@gmail.com>
2020-10-07 17:23:53 +0700

    img upload working on new branch

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const users = require('./routes/api/users');
 const lists = require('./routes/api/lists');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const db = require('./config/my_keys').mongoURI;
+// const fileUpload = require('express-fileupload');
+// const upload = require('./routes/api/upload');
+const cors = require("cors");
+const fileUploadRoutes = require('./routes/api/fileUploadRoutes')
 
 const app = express();
-const db = require('./config/my_keys').mongoURI;
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
@@ -20,6 +25,14 @@ app.use(bodyParser.json());
 
 app.use('/api/users', users);
 app.use('/api/lists', lists);
+// app.use('/api/upload', upload);
+app.use('/api/photo', fileUploadRoutes);
+
+app.use(cors());
+
+// app.use(fileUpload({
+//   createParentPath: true
+// }));
 
 const port = process.env.PORT || 5000;
 
