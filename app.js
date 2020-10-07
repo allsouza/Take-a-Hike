14c71d66a76f5ad7f373afba734eafe9e278c4b2
AAfghahi <arash.afghahi@gmail.com>
2020-10-07 09:07:44 -0400

    added somethings in app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require('mongoose');
 const users = require('./routes/api/users');
 const lists = require('./routes/api/lists');
+const trails = require('./routes/api/trails');
 const reviews = require('./routes/api/reviews');
 const bodyParser = require('body-parser');
 const passport = require('passport');
@@ -22,6 +23,7 @@ app.use(bodyParser.json());
 app.use('/api/users', users);
 app.use('/api/lists', lists);
 app.use('/api/reviews', reviews);
+app.use('/api/trails');
 
 const port = process.env.PORT || 5000;
 
