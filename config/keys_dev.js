6d30aae37375724029ec8efb5e87f7547a290913
Andre Souza <andre.llsouza8@gmail.com>
2020-10-08 09:44:03 -0400

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://dev:FUwC874Uj3PmipMg@mycluster.3nof1.mongodb.net/TakeAHike?retryWrites=true&w=majority",
-    secretOrKey: "1MS6i|;Q}dH,o%",
-    googleMapsApi: 'AIzaSyD82UDrvQNsF2WNrTjXGc7-buTzyFFBrlY'
-  };
- 
